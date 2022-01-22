61b22e38371530712a78e77c11fe4e29a09cbf9f
Ziad <ZiadTalal@gmail.com>
2022-01-22 04:14:22 +0200

    Added login Api

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -11,5 +11,6 @@ router.register('profile',views.UserProfileViewSet)
 
 urlpatterns=[
     path('Hello-View/', views.HelloApiView.as_view()),
+    path('login/',views.UserLoginApiView.as_view()),
     path('', include(router.urls))
 ]
